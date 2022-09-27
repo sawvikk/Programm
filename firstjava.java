cf04b093dc44f839c2807ddec62e2d0a4443e7d3
My  Name <sawvik.dipto10@gmail.com>
2022-09-27 16:51:21 +0600

    4:55pm 27/9/22

--- firstjava.java
+++ firstjava.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 class firstjava{
     public static void main(String[] args){
         System.out.println("Sawvik Kar DIPTO");
-        int a;
-        Scanner in = new Scanner(System.in);
-        a=in.nextInt();
-        System.out.println(a+a+30);
     }
 }
